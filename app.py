742dd965900a29f80fd5e8409ebe1452d46dfe21
Aashna-Paris McGlashin <mcglashinaashnaworks@gmail.com>
2025-01-30 16:39:29 -0500

    Calculating voltage drop of series resistors through a function that takes a list of resistors as an argument and a value for the voltage supply

--- app.py
+++ app.py
@@ -13,3 +13,19 @@ actual_tot = temp ** -1
 
 print(f"{actual_tot:.3f} ohms")
 
+def potential_divider(v_supply, resistors):
+    r_tot = 0
+    for r in resistors:
+        r_tot += r
+        r += 1
+
+    tot_current = v_supply / r_tot
+
+    for x in resistors:
+        v_drop = tot_current * x
+        print(f"{v_drop:.2f}v")
+        
+
+potential_divider(9, [3000, 1000])
+
+
